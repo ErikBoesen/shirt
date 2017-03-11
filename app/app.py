ba7e46ef52943ef47e9b5fa4404aa247bc3e2c92
ErikBoesen <me@erikboesen.com>
2017-03-10 19:53:22 -0500

    Simple inaugural app setup.

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, jsonify
+from flask_pymongo import PyMongo
+import json
+import random
+import string
+import re
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+
+@app.route('/new', methods=['POST'])
+def new():
+    data = request.json
+
+    if data.get('key') is not None:
+        if mongo.db.links.find_one({'key': data['key']}) is None:
+            key = data['key']
+        else:
+            # TODO: This can be done more cleanly.
+            return jsonify({'error': 409}), 409
+    else:
+        key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(0, 4)])
+
+    url = re.sub(r'^https?://(www\.)?', '', data['url'])
+    print('Creating link to %s at key %s...' % (url, key), end='')
+    mongo.db.links.insert({
+        'key': key,
+        'url': url
+    })
+    print(' done.')
+
+    return jsonify({'key': key})
+
+
+@app.route('/<key>')
+def link(key):
+    url = '//' + mongo.db.links.find_one_or_404({'key': key})['url']
+
+    return render_template('redir.html', url=url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
