7fe05e773656248acfb2f5a8afc2eafbdf42d5c6
ErikBoesen <me@erikboesen.com>
2017-03-15 23:34:38 -0400

    Don't make new links for the same URL, and lay some groundwork—currently commented out—for usage tracking.

--- app.py
+++ app.py
@@ -1,43 +1,54 @@
 from flask import Flask, request, render_template, jsonify
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 import json
 import random
 import string
 import re
 
+
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html') #, top=mongo.db.collection.find().sort({uses:-1}).limit(10))
+
 
 @app.route('/new', methods=['POST'])
 def new():
     data = request.json
 
-    if data.get('key') is not None:
-        if db.links.find_one({'key': data['key']}) is None:
-            key = data['key']
+    already = mongo.db.links.find_one({'url': data['url']})
+    if already is None: # If there's not yet a link to that page, then we're going to make one.
+        if data.get('key') is not None:
+            if mongo.db.links.find_one({'key': data['key']}) is None:
+                key = data['key']
+            else:
+                # TODO: This can be done more cleanly.
+                return jsonify({'error': 409}), 409
         else:
-            # TODO: This can be done more cleanly.
-            return jsonify({'error': 409}), 409
-    else:
-        key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(0, 4)])
+            key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(0, 4)])
 
-    url = re.sub(r'^https?://(www\.)?', '', data['url'])
-    print('Creating link to %s at key %s...' % (url, key), end='')
-    db.links.insert({
-        'key': key,
-        'url': url
-    })
-    print(' done.')
+        url = re.sub(r'^https?://(www\.)?', '', data['url'])
+        print('Creating link to %s at key %s...' % (url, key), end='')
+        mongo.db.links.insert({
+            'key': key,
+            'url': url,
+            #'uses': 0
+        })
+        print(' done.')
+    else:
+        key = already['key']
 
     return jsonify({'key': key})
 
 
 @app.route('/<key>')
 def link(key):
-    url = '//' + db.links.find_one({'key': key})['url']
+    url = '//' + mongo.db.links.find_one({'key': key})['url']
 
     return render_template('redir.html', url=url)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
